eaf0d1457a4fabde54ebdd6e25b9dc84d65f9333
Sabyasachi Kumar <2100031988@kluniversity.in>
2026-05-24 18:22:38 +0930

    revising_the_concepts

--- LEARNING_PANDAS.py
+++ LEARNING_PANDAS.py
@@ -108,3 +108,119 @@ df. describe()
 
 df.describe(include='all')
 
+
+
+employee = {
+    'name' : ['sabya', 'vincent', 'sam', 'jeffery', 'ethan'],
+    'id' : [1, 2, 3, 4, 5],
+    'position' : ['ceo', 'ai engineer', 'developer', 'marketing manager', 'cmo'],
+    'experience' : [10, 8, 3, 7, 9],
+    'salary' : [25000, 1300, 2000, 15000, 220000]
+}
+
+df = pd.DataFrame(employee)
+print(df)
+
+dff = pd.read_csv('data.csv')
+dff.head()
+dff.tail()
+dff.info()
+dff.describe()
+dff.describe(include='all')
+
+#  <----------------------------------------------------->
+
+# Accessing Specific Columns
+# suppose we want to extract only the specific column then we can do it as:
+
+# (df['column_name']) 
+# movies.genre / movies['genre'] 
+
+#  <----------------------------------------------------->
+
+# Series
+# It is like a single column of the table [here, the table is like a dataframe as an example]
+
+# type(movies)              # returns pandas.core.frame.DataFrame
+# type(movies['genre'])
+
+#  <----------------------------------------------------->
+
+# Accessing Multiple Columns
+# we can access multiple columns using python's list of column names
+
+# genre_and_studio_df = movies[['genre', 'studio']]
+
+# basic_stats = characters[['name', 'level', 'hp']] # if there is 2,3,4,5,6 any number but bracket only 2
+# print(basic_stats)
+
+# it returns multiple columns
+
+#  <----------------------------------------------------->
+
+# Accessing All But One Column with .drop()
+# it is used when we want to drop everything except one column
+
+# remove_title_df = movies.drop("title", axis=1)
+# it tells pandas that we want to drop title column only.
+
+
+#  <----------------------------------------------------->
+
+
+# Filtering Rows
+# to filter only rows we can do like this:
+
+# long_movies = movies[movies['runtime_movies] > 120]
+
+# we need to understand that why we kept two movies in outside bracket and this comes to
+# our understanding that first it turns those values into true or false like which movie length is
+# larger than 120 minutes and then the outer movies will filter those values and print those which are >120.
+
+#  <----------------------------------------------------->
+
+#  AND and OR
+# You can filter based on multiple conditions by using AND (&) and OR (|).
+# For example, if you wanted to filter for movies that are longer than 120 minutes 
+# AND in the genre 'Sci-Fi'
+
+# long_movies = movies[
+#   (movies['runtime_minutes'] > 120) &
+#   (movies['genre'] == 'Sci-Fi')               # the and operator can be used to check two operations 
+# ]                                             # it is important bro
+
+
+#  <----------------------------------------------------->
+
+
+# Sorting, Renaming, and Adding Columns
+
+# Sorting By Columns with .sort_values()
+# we can sort the values by using .sortvalues method and by a specific column as well.
+
+# box_sorted_values = movies.sort_values(by = 'box_office' , ascending = False)     # descinding order
+
+# box_sorted_values = movies.sort_values(by = 'box_office' , ascending = True)      # ascending order
+
+#  <----------------------------------------------------->
+
+# Rename Columns with .rename()
+# we can change the row name using .rename()
+
+# movies = movies.rename(columns={'budget' : 'budget_usd'})     # here changing budget to budget_usd
+
+# this does not change in the memory meaning not in the table that we are working or datset bro
+# if we want to chnage then we should use inplace = 'true'
+
+# movies = movies.rename(columns={'budget' : 'budget_usd'}, inplace = True) 
+
+#  <----------------------------------------------------->
+
+# Adding Columns
+# we can add new columns by assigning a list or a collection to a column name
+
+# movies['lead_actor'] = ['Keanu Reeves', 'Leonardo DiCaprio', 'Song Kang-ho', 'Matthew Broderick', 'Michelle Yeoh']
+
+# alternatively we can also do it like this way to add budget in millions okk
+
+# movies['budget (millions)'] = movies['budget'] / 1000000
